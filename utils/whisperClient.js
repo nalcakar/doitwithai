7e243c0e410272c31b261a15defa87f05a79be83
nalcakar <nalcakar@gmail.com>
2025-06-08 16:24:55 +0300

    ggüncellendi

--- utils/whisperClient.js
+++ utils/whisperClient.js
@@ -1,10 +1,18 @@
 import fs from 'fs';
 import OpenAI from 'openai';
+import path from 'path';
+
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-export async function transcribeAudio(filePath) {
+export async function transcribeAudio(filePath, originalName) {
+  const stream = fs.createReadStream(filePath);
+  const filename = path.basename(originalName); // e.g. "fsm.mp3"
+
   const resp = await openai.audio.transcriptions.create({
-    file: fs.createReadStream(filePath),
+    file: {
+      name: filename,
+      stream: stream,
+    },
     model: 'whisper-1',
   });
 
