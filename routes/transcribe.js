66f3ceb05561def406f5d6957158161c1663943d
nalcakar <nalcakar@gmail.com>
2025-06-10 23:07:40 +0300

    ggüncellendi

--- routes/transcribe.js
+++ routes/transcribe.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 import path from 'path';
 import ffmpeg from 'fluent-ffmpeg';
 import { transcribeAudio } from '../utils/whisperClient.js';
-import visitorTokenRouter from './visitorTokens.js';
+import { checkVisitorTokens, incrementVisitorUsage } from './visitorTokenUtils.js';
 import axios from 'axios';
 
 const router = express.Router();
